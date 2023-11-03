a765b3861ca80c646f6666cc000928c4674b1c69
Bhuvansh Goyal <bhuvanshgoyal2004@gmail.com>
2023-11-03 13:00:47 +0530

    [fix] reverse shift rows

--- Shift_Rows.vhd
+++ Shift_Rows.vhd
@@ -6,21 +6,21 @@ entity Shift_Rows is
 end entity Shift_Rows;
 
 architecture Struct of Shift_Rows is
-begin
-	output(7 downto 0) <= input(7 downto 0);
-	output(15 downto 8) <= input(47 downto 40);
-	output(23 downto 16) <= input(87 downto 80);
-	output(31 downto 24) <= input(127 downto 120);
-	output(39 downto 32) <= input(39 downto 32);
-	output(47 downto 40) <= input(79 downto 72);
-	output(55 downto 48) <= input(119 downto 112);
-	output(63 downto 56) <= input(31 downto 24);
-	output(71 downto 64) <= input(71 downto 64);
-	output(79 downto 72) <= input(111 downto 104); 
-	output(87 downto 80) <= input(23 downto 16);
-	output(95 downto 88) <= input(63 downto 56);
-	output(103 downto 96) <= input(103 downto 96);
-	output(111 downto 104) <= input(15 downto 8);
-	output(119 downto 112) <= input(55 downto 48);
-	output(127 downto 120) <= input(95 downto 88);
+begin 
+	output(8*16 - 1 downto 8*15) <= input(8*16 - 1 downto 8*15);
+	output(8*15 - 1 downto 8*14) <= input(8*11 - 1 downto 8*10);
+	output(8*14 - 1 downto 8*13) <= input(8*6 - 1 downto 8*5);
+	output(8*13 - 1 downto 8*12) <= input(8*1 - 1 downto 0);
+	output(8*12 - 1 downto 8*11) <= input(8*12 - 1 downto 8*11);
+	output(8*11 - 1 downto 8*10) <= input(8*7 - 1 downto 8*6);
+	output(8*10 - 1 downto 8*9) <= input(8*2 - 1 downto 8*1);
+	output(8*9 - 1 downto 8*8) <= input(8*13 - 1 downto 8*12);
+	output(8*8 - 1 downto 8*7) <= input(8*8 - 1 downto 8*7);
+	output(8*7 - 1 downto 8*6) <= input(8*3 - 1 downto 8*2); 
+	output(8*6 - 1 downto 8*5) <= input(8*14 - 1 downto 8*13);
+	output(8*5 - 1 downto 8*4) <= input(8*9 - 1 downto 8*8);
+	output(8*4 - 1 downto 8*3) <= input(8*4 - 1 downto 8*3);
+	output(8*3 - 1 downto 8*2) <= input(8*15 - 1 downto 8*14);
+	output(8*2 - 1 downto 8*1) <= input(8*10 - 1 downto 8*9);
+	output(8*1 - 1 downto 0) <= input(8*5 - 1 downto 8*4);
 end Struct;
